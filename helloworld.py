89055f64ea08f5ef12ca453e846ef62dc2986df6
Johan Thorell <johan.thorell@propellerheads.se>
2019-05-19 18:14:40 +0200

    first commit

---
+++ helloworld.py
@@ -0,0 +1,5 @@
+def your_first_command(debugger, command, result, internal_dict):
+    import pdb; pdb.set_trace()
+    print("Hello world!")
+def __lldb_init_module(debugger, internal_dict):
+    debugger.HandleCommand('command script add -f helloworld.your_first_command yay')
