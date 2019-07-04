90051223385ef117467b502436077cbc6ef1d5fd
Johan Thorell <jo1han@hotmail.com>
2019-07-04 15:55:39 +0200

    updated mom script

--- mom.py
+++ mom.py
@@ -8,7 +8,12 @@ import optparse
 def __lldb_init_module(debugger, internal_dict):
     debugger.HandleCommand(
     'command script add -f mom.handle_command print_mom')
+def prepend(List, str):
 
+    # Using format()
+    str += '{0}'
+    List = ((map(str.format, List)))
+    return List
 def handle_command(debugger, command, result, internal_dict):
     '''
     Documentation for how to use mom goes here
@@ -22,10 +27,18 @@ def handle_command(debugger, command, result, internal_dict):
         result.SetError(parser.usage)
         return
 
-    # Uncomment if you are expecting at least one argument
-    # clean_command = shlex.split(args[0])[0]
+    commandString = 'po [GetModelBridgeInstance() getMOM:@[]]'
+
+    if options.keys:
+        keys = [x.strip() for x in options.keys.replace('"', '').split(',')]
+        prepended = prepend(keys, '@"')
+        appended = [s + '",' for s in prepended]
+        string = ''.join(appended)
+        string = string[:-1]
+        commandString = 'po [GetModelBridgeInstance() getMOM:@[{}]]'.format(string)
+
     res = lldb.SBCommandReturnObject()
-    lldb.debugger.GetCommandInterpreter().HandleCommand("po [GetMainViewModelInstance() getMOM:(NSArray*)@[]]", res)
+    lldb.debugger.GetCommandInterpreter().HandleCommand(commandString, res)
     returnVal = res.GetOutput()
     result.AppendMessage(returnVal)
 
@@ -33,15 +46,10 @@ def handle_command(debugger, command, result, internal_dict):
 def generateOptionParser():
     usage = "usage: %prog [options] TODO Description Here :]"
     parser = optparse.OptionParser(usage=usage, prog="mom")
-    parser.add_option("-m", "--module",
+    parser.add_option("-k", "--keys",
                       action="store",
                       default=None,
-                      dest="module",
+                      dest="keys",
                       help="This is a placeholder option to show you how to use options with strings")
-    parser.add_option("-c", "--check_if_true",
-                      action="store_true",
-                      default=False,
-                      dest="store_true",
-                      help="This is a placeholder option to show you how to use options with bools")
     return parser
 
